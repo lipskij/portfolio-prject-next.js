8adb699f6d62304d54f78f28978687489bf4c646
Emil Lipskij <emil.lipskij@gmail.com>
2021-02-01 20:57:53 +0200

    to add enlarge functionality

--- component/GalleryTwo.js
+++ component/GalleryTwo.js
@@ -1,23 +1,18 @@
-import React, { useState } from 'react';
-import { FetchData } from './ImageData';
+import React, { useState } from "react";
+import { FetchData } from "./ImageData";
 
 // SameSite='Lax' some sort of cookie thing
 // onClick function to inlarge image
 // on click logs all images
 
 const GalleryTwo = () => {
-  const [active, setActive] = useState(false);
 
   return (
     <section className="gal-two">
       {FetchData.map((img) => {
-        console.log(img.url);
         return (
           <div key={img.url}>
-            {active ? console.log('clicked') : console.log('inacive')}{' '}
-            <button className="img-btn" onClick={() => setActive(true)}>
               <img className="fetch-img" src={img.url} key={img.url} />
-            </button>
           </div>
         );
       })}
