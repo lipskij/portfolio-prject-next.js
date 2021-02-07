3bd89ec8585561b1faf15afb37143469c901e55c
Emil Lipskij <emil.lipskij@gmail.com>
2021-02-07 12:42:14 +0200

    link err fix

--- pages/more.js
+++ pages/more.js
@@ -8,8 +8,6 @@ import { FaGithubAlt } from "react-icons/fa";
 import { HiOutlineMail } from "react-icons/hi";
 import { GrLinkedin } from "react-icons/gr";
 
-// https://nextjs.org/docs/api-reference/next/link#if-the-child-is-a-custom-component-that-wraps-an-a-tag
-
 const More = () => {
   return (
     <>
@@ -24,20 +22,29 @@ const More = () => {
           <div className={styles.text}>
             <p className="more-txt">
               To find more projects that i did{" "}
-              <Link href="https://github.com/lipskij">
-                <FaGithubAlt className="git-logo" size={30} />
+              <Link href="https://github.com/lipskij" target="_blank">
+                <a>
+                  <FaGithubAlt className="git-logo" size={30} alt="logo" />
+                </a>
               </Link>
             </p>
             <p className="more-txt">
               Write me an email
-              <Link href="mailto:emil.lipskij@gmail.com">
-                <HiOutlineMail className="mail-logo" size={30} />
+              <Link href="mailto:emil.lipskij@gmail.com" target="_blank">
+                <a>
+                  <HiOutlineMail className="mail-logo" size={30} alt="logo" />
+                </a>
               </Link>
             </p>
             <p className="more-txt">
               Or contact me on
-              <Link href="https://www.linkedin.com/in/emil-lipskij-6920831b2">
-                <GrLinkedin className="fb-logo" size={25} />
+              <Link
+                href="https://www.linkedin.com/in/emil-lipskij-6920831b2"
+                target="_blank"
+              >
+                <a>
+                  <GrLinkedin className="fb-logo" size={25} alt="logo" />
+                </a>
               </Link>
             </p>
           </div>
