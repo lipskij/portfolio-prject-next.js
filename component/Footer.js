e5bc026211c4fa591c53dd8563c7929d1f54d7ed
Emil Lipskij <emil.lipskij@gmail.com>
2021-04-08 14:08:07 +0300

    Fix footer

--- component/Footer.js
+++ component/Footer.js
@@ -1,28 +1,26 @@
 import React from "react";
-import styles from "../styles/Home.module.css";
-import { moreBottom } from "../styles/contacts.module.css";
 import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer className={styles.footer}>
-      <ul className={moreBottom}>
+    <footer className='footer'>
+      <ul className='moreBottom'>
         <li>Contact number +370 00 00 000</li>
         <li>Visit www.link.com</li>
         <li>Terms and condisions</li>
       </ul>
-      <ul className={moreBottom}>
+      <ul className='moreBottom'>
         <li>About</li>
         <li>Projects</li>
         <li>Privacy policy</li>
       </ul>
-      <ul className={moreBottom}>
+      <ul className='moreBottom'>
         <li>Help</li>
         <li>FAQ</li>
         <li>Blog</li>
       </ul>
-      <div className={styles.credit}>
-        <p className={styles.madeBy}>
+      <div className='credit'>
+        <p className='madeBy'>
           <Link rel='noreferre' href='https://lipskij.xyz/'>
             <a target='_balnk'>
               <i>Made by Emil Lipskij</i>
