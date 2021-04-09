a7c04b82dca527ed4aedeb80d75a1d66ab72f74d
Emil Lipskij <emil.lipskij@gmail.com>
2021-04-09 14:22:32 +0300

    Add nav logo

--- component/Nav.js
+++ component/Nav.js
@@ -43,7 +43,43 @@ const Nav = () => {
           </li>
         </ul>
       </Drawer>
-      <p className='logo'>{`< generic logo />`}</p>
+      <div className='navSVG'>
+        <svg
+          width='34'
+          height='34'
+          viewBox='0 0 340 340'
+          fill='none'
+          xmlns='http://www.w3.org/2000/svg'
+        >
+          <line
+            x1='236.5'
+            y1='237.004'
+            x2='236.5'
+            y2='103'
+            stroke='white'
+            strokeWidth='5'
+          />
+          <rect
+            x='85'
+            y='94'
+            width='171'
+            height='152'
+            stroke='white'
+            strokeWidth='2'
+          />
+          <circle cx='170.5' cy='169.5' r='134' stroke='white' />
+          <circle cx='170.5' cy='169.5' r='144' stroke='white' />
+          <circle cx='170' cy='170' r='169.5' stroke='white' />
+          <rect
+            x='102'
+            y='107'
+            width='117'
+            height='126'
+            stroke='white'
+            strokeWidth='2'
+          />
+        </svg>
+      </div>
       <ul className='desctop-ul'>
         <li>
           <Link href='/'>
